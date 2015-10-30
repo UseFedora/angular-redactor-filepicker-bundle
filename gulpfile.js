97b7cb0215b1bf7c61fd80342ecff46a6d79f3dc
Noah Pryor <noah@noahpryor.com>
2015-10-29 21:19:57 -0400

    Update updateModel method

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-  libraryPaths = ['src/redactor.js','src/redactor-filepicker.js','src/redactor-fullscreen.js',"src/angular-redactor.js"]
+  libraryPaths = [ 'src/redactor.js','src/redactor-filepicker.js','src/redactor-fullscreen.js',"src/angular-redactor.js"]
   stream = gulp.src(libraryPaths)
     .pipe(concat("angular-redactor-filepicker.js"))
     .pipe(gulp.dest("."));
--- src/angular-redactor.js
+++ src/angular-redactor.js
@@ -1,161 +1,3 @@
-// if (!RedactorPlugins) var RedactorPlugins = {};
-
-// if (!RedactorPlugins) var RedactorPlugins = {};
-
-
-//   RedactorPlugins.fullscreen = function()
-//   {
-//     return {
-//       init: function()
-//       {
-//         this.fullscreen.isOpen = false;
-
-//         var button = this.button.add('fullscreen', 'Fullscreen');
-//         this.button.addCallback(button, this.fullscreen.toggle);
-
-//         if (this.opts.fullscreen) this.fullscreen.toggle();
-//       },
-//       enable: function()
-//       {
-//         this.button.changeIcon('fullscreen', 'normalscreen');
-//         this.button.setActive('fullscreen');
-//         this.fullscreen.isOpen = true;
-
-//         if (this.opts.toolbarExternal)
-//         {
-//           this.fullscreen.toolcss = {};
-//           this.fullscreen.boxcss = {};
-//           this.fullscreen.toolcss.width = this.$toolbar.css('width');
-//           this.fullscreen.toolcss.top = this.$toolbar.css('top');
-//           this.fullscreen.toolcss.position = this.$toolbar.css('position');
-//           this.fullscreen.boxcss.top = this.$box.css('top');
-//         }
-
-//         this.fullscreen.height = this.$editor.height();
-
-//         if (this.opts.maxHeight) this.$editor.css('max-height', '');
-//         if (this.opts.minHeight) this.$editor.css('min-height', '');
-
-//         if (!this.$fullscreenPlaceholder) this.$fullscreenPlaceholder = $('<div/>');
-//         this.$fullscreenPlaceholder.insertAfter(this.$box);
-
-//         this.$box.appendTo(document.body);
-
-//         this.$box.addClass('redactor-box-fullscreen');
-//         $('body, html').css('overflow', 'hidden');
-
-//         this.fullscreen.resize();
-//         $(window).on('resize.redactor.fullscreen', $.proxy(this.fullscreen.resize, this));
-//         $(document).scrollTop(0, 0);
-
-//         $('.redactor-toolbar-tooltip').hide();
-//         this.$editor.focus();
-//         this.observe.load();
-//       },
-//       disable: function()
-//       {
-//         this.button.removeIcon('fullscreen', 'normalscreen');
-//         this.button.setInactive('fullscreen');
-//         this.fullscreen.isOpen = false;
-
-//         $(window).off('resize.redactor.fullscreen');
-//         $('body, html').css('overflow', '');
-
-//         this.$box.insertBefore(this.$fullscreenPlaceholder);
-//         this.$fullscreenPlaceholder.remove();
-
-//         this.$box.removeClass('redactor-box-fullscreen').css({ width: 'auto', height: 'auto' });
-
-//         this.code.sync();
-
-//         if (this.opts.toolbarExternal)
-//         {
-//           this.$box.css('top', this.fullscreen.boxcss.top);
-//           this.$toolbar.css({
-//             'width': this.fullscreen.toolcss.width,
-//             'top': this.fullscreen.toolcss.top,
-//             'position': this.fullscreen.toolcss.position
-//           });
-//         }
-
-//         if (this.opts.minHeight) this.$editor.css('minHeight', this.opts.minHeight);
-//         if (this.opts.maxHeight) this.$editor.css('maxHeight', this.opts.maxHeight);
-
-//         $('.redactor-toolbar-tooltip').hide();
-//         this.$editor.css('height', 'auto');
-//         this.$editor.focus();
-//         this.observe.load();
-//       },
-//       toggle: function()
-//       {
-//         if (this.fullscreen.isOpen)
-//         {
-//           this.fullscreen.disable();
-//         }
-//         else
-//         {
-//           this.fullscreen.enable();
-//         }
-//       },
-//       resize: function()
-//       {
-//         if (!this.fullscreen.isOpen) return;
-
-//         var toolbarHeight = this.$toolbar.height();
-
-//         var height = $(window).height() - toolbarHeight - this.utils.normalize(this.$editor.css('padding-top')) - this.utils.normalize(this.$editor.css('padding-bottom'));
-//         this.$box.width($(window).width()).height(height);
-
-//         if (this.opts.toolbarExternal)
-//         {
-//           this.$toolbar.css({
-//             'top': '0px',
-//             'position': 'absolute',
-//             'width': '100%'
-//           });
-
-//           this.$box.css('top', toolbarHeight + 'px');
-//         }
-
-//         this.$editor.height(height);
-//       }
-//     };
-//   };
-
-// RedactorPlugins.filepicker = function() {
-//   return {
-//     init: function() {
-//       var button = this.button.add('filepicker', 'Add Image');
-//       this.button.addCallback(button, this.filepicker.show);
-
-//       // make your added button as Font Awesome's icon
-//       this.button.setAwesome('filepicker', 'fa-image');
-//     },
-//     show: function() {
-//       filepicker.pick(this.filepicker.insert, {
-//         mimetype: 'image/*',
-//         container: 'modal',
-//         services: [
-//           "COMPUTER",
-//           "IMAGE_SEARCH",
-//           "URL",
-//           "FTP"
-//         ]
-//       });
-
-//     },
-//     insert: function(object) {
-//       html = "<img src='" + object.url + "' class='teachable-image'>"
-
-//       this.insert.html(html);
-
-//       this.code.sync();
-
-//     }
-//   };
-// };
-
-
 
     /**
      * usage: <textarea ng-model="content" redactor></textarea>
@@ -173,24 +15,23 @@
                 require: 'ngModel',
                 link: function(scope, element, attrs, ngModel) {
 
-                    // Expose scope var with loaded state of Redactor
+                  // Expose scope var with loaded state of Redactor
                     scope.redactorLoaded = false;
 
-                    var updateModel = function updateModel(value) {
-                            // $timeout to avoid $digest collision
-                            $timeout(function() {
-                                scope.$apply(function() {
-                                    ngModel.$setViewValue(value);
-                                });
-                            });
+                        var updateModel = function() {
+                            scope.$apply(ngModel.$setViewValue($_element.redactor('code.get')));
                         },
                         options = {
-                            changeCallback: updateModel,
-                            replaceDivs: false,
-                            paragraphize: false,
-                            deniedTags: [],
-                            plugins: ['filepicker', 'fullscreen'],
-                            buttonSource: true
+                            keyupCallback: updateModel,
+                            keydownCallback: updateModel,
+                            execCommandCallback: updateModel,
+                            autosaveCallback: updateModel,
+                            focusCallback: updateModel,
+                            blurCallback: updateModel,
+                            plugins: ['filepicker', 'fullscreen', 'fontcolor', 'fontsize', 'fontfamily'],
+                            buttons: ['html', 'formatting', 'bold', 'italic', 'underline', 'orderedlist', 'unorderedlist', 'outdent', 'indent', 'image', 'file', 'link', 'alignment', 'horizontalrule'],
+                            deniedTags: ['html', 'head', 'body', 'meta', 'applet'],
+                            replaceDivs: false
                         },
                         additionalOptions = attrs.redactor ?
                             scope.$eval(attrs.redactor) : {},
@@ -227,5 +68,3 @@
                 }
             };
         }]);
-
-
